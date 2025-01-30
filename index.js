6f8eeec71f0055354ecb7d8330aa8e43cface442
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-01-30 23:05:29 +0600

    rseponse to the client is added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const http = require("http");
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  res.end("Assalamu Alaikum, the server has started!");
+});
 
 server.listen(8000, "localhost", () => {
   console.log("Server is started!");
