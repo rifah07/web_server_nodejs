076e83b4c773a0b7414d235922c08510becfa143
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-01-31 23:38:17 +0600

    Dynamic Routing Added

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Assalamu Alaikum, the server has started!");
+  console.log(req.url);
+
+  if (req.url === "/mars") {
+    res.end("This page is for Mars url");
+  } else if (req.url === "/flowers") {
+    res.end("This page is for Folwers url");
+  } else {
+    res.end("Assalamu Alaikum, the server has started!");
+  }
 });
 
 server.listen(8000, "localhost", () => {
