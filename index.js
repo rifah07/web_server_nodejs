121839338ea1410172776769c3189bc5c7af20e2
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-02-07 00:01:17 +0600

    before listening port, in get a message has been sent

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Assalamu Alaikum from the server!");
+});
+
 app.listen(8000, () => {
   console.log("Server is running now!");
 });
