1e1f493f72fbb4079962b44e5049132cb6b5e98e
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-02-07 00:07:17 +0600

    response for multimple different page is added

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get("/", (req, res) => {
   res.send("Assalamu Alaikum from the server!");
 });
 
+app.get("/mars", (req, res) => {
+  res.send("Hi, this is from mars.");
+});
+
+app.get("/flowers", (req, res) => {
+  res.send("Hi, this is from flowers.");
+});
+
 app.listen(8000, () => {
   console.log("Server is running now!");
 });
