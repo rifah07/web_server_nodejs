1f134d151117ded1177e64f36da47b277a45b8e5
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-02-08 23:03:34 +0600

    nodemon activated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Assalamu Alaikum from the server!");
+  res.send("Assalamu Alaikum from the server! This is Rifah.");
 });
 
 app.get("/mars", (req, res) => {
